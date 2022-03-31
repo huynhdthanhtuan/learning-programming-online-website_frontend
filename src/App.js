bd0a5f3a9f14461aef99ea75878aa202131d1147
huynhdthanhtuan <tuan40655@gmail.com>
2022-04-01 01:18:07 +0700

    up source

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import "./App.css";
+import React, { useReducer, useContext, createContext } from "react";
+import GlobalStyle from "./Components/GlobalStyle"
+import Root from './Components/Screen/root'
+import { BrowserRouter } from "react-router-dom";
+import { reducer, initialState } from "./reducers";
+
+
+export const UserContext = createContext();
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+   
+      <GlobalStyle>
+        <UserContext.Provider value={{ state, dispatch }}>  
+            <Root />     
+        </UserContext.Provider>
+      </GlobalStyle>
+   
+  );
+}
+export default App;
