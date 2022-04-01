25b4739348a9bc064f9b85c68a6b25ae16fbb26d
huynhdthanhtuan <tuan40655@gmail.com>
2022-04-01 21:40:25 +0700

    up source 8

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  //...
-  devServer: {
-    allowedHosts: ["https://learning-programming-online-be.herokuapp.com/"],
-  },
+  // devServer: {
+  //   allowedHosts: ["https://learning-programming-online-be.herokuapp.com/"],
+  // },
+  devServer: { host: "0.0.0.0", disableHostCheck: true },
 };
